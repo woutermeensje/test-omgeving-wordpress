e39ded1bfdb71b3ab166f400d4cd81c7338d613f
WouterMeens <w.m.meens@gmail.com>
2025-05-06 17:20:08 +0200

    plugin verwijderd

--- wp-content/plugins/mailtrap-for-wp/includes/notices.php
+++
@@ -1,5 +0,0 @@
-<?php if($email_sent === true): ?>
-    <div class="notice notice-success is-dismissible">
-        <p><?php echo __( 'Your email has been sent successfully', 'mailtrap-for-wp' ) ?></p>
-    </div>
-<?php endif; ?>
